46d639ee9e50db69b2b380f3cff18339bdcc0c35
Konstantin <nemeshkv@gmail.com>
2023-11-10 23:23:05 +0500

    Initial commit

---
+++ src/ru/nemesh/Main.java
@@ -0,0 +1,20 @@
+package ru.nemesh;
+
+import javax.servlet.http.HttpServlet;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        String keyapi = "5ba4a553d3605f0f89a02b2e0cc0182300c8bab8";
+        String result;
+        boolean flag = true;
+
+        System.out.println("ֲגוהטעו ָֽֽ");
+    }
+}
